fe906b46a2140f94851cecd8efe84de06cbeb3ae
Rishikesh-kumar-7258 <rishi7258prince@gmail.com>
2022-03-29 13:30:22 +0530

    Configured react

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import User
+from .models import User, Scheme, Statistics
 
 # Register your models here.
 admin.site.register(User)
+admin.site.register(Scheme)
+admin.site.register(Statistics)
