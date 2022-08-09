edaeb3df16e2911293599f91e527a02e0ca02fe5
Rishikesh-kumar-7258 <rishi7258prince@gmail.com>
2022-08-09 17:40:13 +0530

    Starting fresh

--- backendSIH/app/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Document, Profile, Scheme
-
-# Register your models here.
-admin.site.register(Document)
-admin.site.register(Scheme)
-admin.site.register(Profile)
--- backendSIH/app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from .views import addScheme, index, signupForm, addDocument
-
-urlpatterns = [
-    path('', index, name='index'),
-    path('signup/', signupForm, name="signup"),
-    path('addScheme/', addScheme, name='addScheme'),
-    path('addDocument/', addDocument, name='addDocument'),
-]
